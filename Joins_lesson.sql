2e7babba6537c7db9026102f40cb137ecb276bd2
Валентина Скирська <126077507+Tinalike17@users.noreply.github.com>
2023-05-03 23:24:22 +0300

    Create Joins_lesson.sql

---
+++ Joins_lesson.sql
@@ -0,0 +1,48 @@
+
+create table City(
+id serial,
+title varchar(50)
+)
+
+create table Person(
+id serial,
+person_name varchar(50),
+city_id int
+)
+
+select*from City;
+
+insert into City(title)
+values  ('Berlin'),
+		('Tokio'),
+		('Antalya'),
+		('Paris'),
+		('Roma')
+
+insert into Person(person_name, city_id)
+values ('Victor',1),
+('Elena',2),
+('Anna', 3),
+('Vadim', 3),
+('Ivan', 7),
+('Irina', 9)
+
+select*from Person inner join city 
+on person.city_id = City.id;
+
+select person_name, title, city_id, city.id from 
+Person inner join city 
+on person.city_id = City.id;
+
+select p.person_name, c.title, c.id from
+person as p inner join city as c 
+on p.city_id = c.id;
+
+select person.person_name, person.city_id, city.title, city.id from
+person left join city
+on person.city_id = City.id;
+
+
+
+
+
